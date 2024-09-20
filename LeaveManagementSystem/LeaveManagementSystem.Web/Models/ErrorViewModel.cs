033e9e40b76fd686439a7d0a71cb97b763729a50
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2024-09-20 13:47:55 -0400

    scaffold project

---
+++ LeaveManagementSystem/LeaveManagementSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagementSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
